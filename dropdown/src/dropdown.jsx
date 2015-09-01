baab88ff25f97954bc0f2da82d3875d9a1f7adb6
sunkay <sunkay11@gmail.com>
2015-08-31 21:50:48 -0400

    dropdown...

--- dropdown/src/dropdown.jsx
+++ dropdown/src/dropdown.jsx
@@ -2,17 +2,33 @@
 
 var React = require('react');
 var Button = require('./button');
-var List = require('./list');
+var ListItem = require('./list-item');
 
 module.exports = React.createClass({
+  handleClick: function(){
+    // setState forces the component to re-render
+    this.setState({open: !this.state.open});
+  },
+
+  getInitialState: function(){
+      return {open: false}
+  },
+
   render: function(){
+    var list = this.props.items.map(function(item){
+      return <ListItem item={item} />
+    });
+
 
     return <div className="dropdown">
       <Button title={this.props.title}
               className="btn-default"
               subTitleClassName="caret"
+              whenClicked={this.handleClick}
         />
-      <List />
+      <ul className={"dropdown-menu " + (this.state.open ? " show " : " ")}>
+        {list}
+      </ul>
     </div>
   }
 });
